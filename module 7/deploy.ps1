f787d898b47d01f34f9f808aad5b2d3a8373fd0b
Yannick Dils <yannick.dils@aspex.be>
2022-02-16 11:33:35 +0100

    Updated files in demo scripts

--- module 7/deploy.ps1
+++ module 7/deploy.ps1
@@ -7,7 +7,7 @@ Login-AzAccount -Subscription $SubscriptionID
 # Create the service principal - Run this account in Azure CLI
 $SubscriptionID = "5c58c666-d960-46d6-8242-c1073d5df2b1"
 $ResourceGroupInitials = "ydi"
-az ad sp create-for-rbac --name $($ResourceGroupInitials) --role contributor --scopes "/subscriptions/$($SubscriptionID)/resourceGroups/hogent-$($ResourceGroupInitials)-rg" --sdk-auth
+az ad sp create-for-rbac --name ydiapp --role contributor --scopes "/subscriptions/5c58c666-d960-46d6-8242-c1073d5df2b1/resourceGroups/hogent-ydi-rg" --sdk-auth
 
 # Run the following command to deploy the bicep template
 
